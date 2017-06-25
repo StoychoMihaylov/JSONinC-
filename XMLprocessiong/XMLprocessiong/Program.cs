b7f976a24f41c5b56b742e69e3f75aa694f69a17
StoychoMihaylov <100ycho100@gmail.com>
2017-06-25 10:11:13 +0300

    ADD XML Processiong

---
+++ XMLprocessiong/XMLprocessiong/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace XMLprocessiong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument xmlDoc = XDocument.Parse(@"<?xml version=""1.0""?><Root><Child>Value</Child></Root>");
+
+            XDocument xmlSecondDoc = XDocument.Load("../../XMLCars.xml");
+            var cars = xmlSecondDoc.Root.Elements();
+            //cars.Remove();
+            foreach (var car in cars)
+            {
+                var make = car.Element("make").Value;
+                var model = car.Element("model").Value;
+
+                //car.Element("traveled-distance").Value = "100";
+                //car.SetElementValue("travel-distance", "100");
+                //car.Remove();
+                //car.Attribute("model").Value;
+                Console.WriteLine($"{make} - {model}");
+            }
+
+            var cars2 = xmlSecondDoc.Root.Elements()
+                .Where(e => e.Element("make").Value == "Opel" &&
+                (long)e.Element("travelled-distance") >= 300000)
+                .Select(c => new
+                {
+                    Model = c.Element("model").Value,
+                    Traveled = c.Element("travelled-distance").Value
+                }).ToList();
+
+            foreach (var car in cars2)
+            {
+                Console.WriteLine($"{car.Model} - {car.Traveled}");
+            }
+
+            //---------------------------
+
+            XDocument xmlBookDoc = new XDocument();
+            xmlBookDoc.Add(
+                new XElement("books",
+                new XElement("book",
+                new XElement("author", "Don Box"),
+                new XElement("title", "ASP.NET", new XElement("lang", "en")))));
+            xmlBookDoc.Save("../../Books.xml", SaveOptions.DisableFormatting);
+
+            //serialize to object
+            var serializer = new XmlSerializer(typeof(object));
+            var writer = new StreamWriter("myProduct.xml");
+            using (writer)
+            {
+                serializer.Serialize(writer, product);
+            }
+        }
+    }
+}
---
+++ XMLprocessiong/XMLserializer/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace XMLserializer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var product = new ProductDTO
+            {
+                Name = "Transmission",
+                Cost = 800m
+            };
+
+            var serializer = new XmlSerializer(product.GetType());
+            var writer = new StreamWriter("../../product.xml");
+            using (writer)
+            {
+                serializer.Serialize(writer, product);
+            }
+
+            var reader = new StreamReader("../../product.xml");
+            var prd = (ProductDTO)serializer.Deserialize(reader);
+
+            Console.WriteLine(prd.Name);
+            Console.WriteLine(prd.Cost);
+        }
+    }
+
+    public class ProductDTO
+    {
+        public string Name { get; set; }
+
+        public decimal Cost { get; set; }
+    }
+}
